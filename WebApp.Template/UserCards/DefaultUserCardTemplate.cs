9693e5fb95e75a6610233a95b3dba5f094eed8cf
lozturkMobven <levent.ozturk@mobven.com>
2022-11-08 09:58:30 +0300

    added default and prime user card templates

---
+++ WebApp.Template/UserCards/DefaultUserCardTemplate.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApp.Template.UserCards
+{
+    public class DefaultUserCardTemplate : UserCardTemplate
+    {
+        protected override string SetFooter()
+        {
+            return String.Empty;
+        }
+
+        protected override string SetPicture()
+        {
+            return $"<img src='/userpictures/Sample_User_Icon.png' class='card-img-top'>";
+        }
+    }
+}
---
+++ WebApp.Template/UserCards/PrimeUserCardTemplate.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+namespace WebApp.Template.UserCards
+{
+    public class PrimeUserCardTemplate : UserCardTemplate
+    {
+        protected override string SetFooter()
+        {
+            var sb = new StringBuilder();
+            sb.Append("<a href='#'class='card-link'>Send Message</a>");
+            sb.Append("<a href='#'class='card-link'>Profile Detail</a>");
+            return sb.ToString();
+        }
+
+        protected override string SetPicture()
+        {
+            return $"<img src='{AppUser.PictureUrl}' class='card-img-top'>";
+        }
+    }
+}
