91e7ce36a73ba32057752ebbf457e1dd48d0b773
lozturkMobven <levent.ozturk@mobven.com>
2022-11-08 10:24:04 +0300

    completed template design pattern

--- WebApp.Template/Program.cs
+++ WebApp.Template/Program.cs
@@ -37,11 +37,11 @@ identityDbContext.Database.Migrate();
 
 if (!userManager.Users.Any())
 {
-    userManager.CreateAsync(new AppUser() { UserName = "user1", Email = "user1@mail.com" }, "123456Aa*").Wait();
-    userManager.CreateAsync(new AppUser() { UserName = "user2", Email = "user2@mail.com" }, "123456Aa*").Wait();
-    userManager.CreateAsync(new AppUser() { UserName = "user3", Email = "user3@mail.com" }, "123456Aa*").Wait();
-    userManager.CreateAsync(new AppUser() { UserName = "user4", Email = "user4@mail.com" }, "123456Aa*").Wait();
-    userManager.CreateAsync(new AppUser() { UserName = "user5", Email = "user5@mail.com" }, "123456Aa*").Wait();
+    userManager.CreateAsync(new AppUser() { UserName = "user1", Email = "user1@mail.com", PictureUrl= "/userpictures/icon-256x256.png", Description= "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." }, "123456Aa*").Wait();
+    userManager.CreateAsync(new AppUser() { UserName = "user2", Email = "user2@mail.com", PictureUrl = "/userpictures/icon-256x256.png", Description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." }, "123456Aa*").Wait();
+    userManager.CreateAsync(new AppUser() { UserName = "user3", Email = "user3@mail.com", PictureUrl = "/userpictures/icon-256x256.png", Description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." }, "123456Aa*").Wait();
+    userManager.CreateAsync(new AppUser() { UserName = "user4", Email = "user4@mail.com", PictureUrl = "/userpictures/icon-256x256.png", Description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." }, "123456Aa*").Wait();
+    userManager.CreateAsync(new AppUser() { UserName = "user5", Email = "user5@mail.com", PictureUrl = "/userpictures/icon-256x256.png", Description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." }, "123456Aa*").Wait();
 }
 
 app.Run();
---
+++ WebApp.Template/UserCards/UserCardTagHelper.cs
@@ -0,0 +1,29 @@
+﻿using BaseProject.Models;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace WebApp.Template.UserCards
+{
+    public class UserCardTagHelper : TagHelper
+    {
+        public AppUser AppUser{ get; set; }
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public UserCardTagHelper(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            UserCardTemplate template;
+            if (_httpContextAccessor.HttpContext.User.Identity.IsAuthenticated)
+                template = new PrimeUserCardTemplate();
+            else
+                template = new DefaultUserCardTemplate();
+
+            template.SetUser(AppUser);
+
+            output.Content.SetHtmlContent(template.Build());
+        }
+    }
+}
