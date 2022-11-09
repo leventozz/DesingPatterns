829c2848bf348b9d99e4197f341c7b98b2269652
leventozz <leventufukozturk@gmail.com>
2022-11-09 10:47:38 +0300

    added excel creator

---
+++ WebApp.Command/Command/ExcelFile.cs
@@ -0,0 +1,45 @@
+﻿using ClosedXML.Excel;
+using System.Data;
+
+namespace WebApp.Command.Command
+{
+    public class ExcelFile<T>
+    {
+        public readonly List<T> _list;
+        public string FileName => $"{typeof(T).Name}.xlsx";
+        public string FileType => "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+
+        public ExcelFile(List<T> list)
+        {
+            _list = list;
+        }
+
+        public MemoryStream Create()
+        {
+            var wb = new XLWorkbook();
+            var ds = new DataSet();
+
+            ds.Tables.Add(GetTable());
+            wb.Worksheets.Add(ds);
+
+            var excelMemory = new MemoryStream();
+            wb.SaveAs(excelMemory);
+
+            return excelMemory;
+        }
+
+        private DataTable GetTable()
+        {
+            DataTable dt = new DataTable();
+            var type = typeof(T);
+            type.GetProperties().ToList().ForEach(p => dt.Columns.Add(p.Name, p.PropertyType));
+
+            _list.ForEach(x =>
+            {
+                var values = type.GetProperties().Select(p => p.GetValue(x,null)).ToArray();
+                dt.Rows.Add(values);
+            });
+            return dt;
+        }
+    }
+}
