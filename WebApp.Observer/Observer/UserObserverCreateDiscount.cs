9fc61082b641b6bdb844f6812bcb31aaa8dc41b3
leventozz <leventufukozturk@gmail.com>
2022-11-25 11:11:39 +0300

    added observers

---
+++ WebApp.Observer/Observer/UserObserverCreateDiscount.cs
@@ -0,0 +1,24 @@
+﻿using BaseProject.Models;
+
+namespace WebApp.Observer.Observer
+{
+    public class UserObserverCreateDiscount : IUserObserver
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public UserObserverCreateDiscount(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public void CreateUser(AppUser appUser)
+        {
+            var logger = _serviceProvider.GetRequiredService<ILogger<UserObserverCreateDiscount>>();
+            var context = _serviceProvider.GetRequiredService<AppIdentityDbContext>();
+
+            context.Discounts.Add(new Models.Discount { Rate=10, UserId=appUser.Id });
+            context.SaveChanges();
+            logger.LogInformation("Discount created");
+        }
+    }
+}
---
+++ WebApp.Observer/Observer/UserObserverSendEMail.cs
@@ -0,0 +1,23 @@
+﻿using BaseProject.Models;
+
+namespace WebApp.Observer.Observer
+{
+    public class UserObserverSendEMail : IUserObserver
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public UserObserverSendEMail(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public void CreateUser(AppUser appUser)
+        {
+            var logger = _serviceProvider.GetRequiredService<ILogger<UserObserverSendEMail>>();
+
+            // send email logic
+
+            logger.LogInformation("Email sent");
+        }
+    }
+}
